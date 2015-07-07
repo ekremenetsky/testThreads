4ea798ac61c661a199e1f0c101c268dbafa2f766
Evgeny Kremenetsky <ekremenetsky@dev.rtsoft.ru>
2015-07-07 13:07:57 +0300

    Main: added main.c

---
+++ task1/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void f(int a) {
+        printf("f function, a = %d\n", a);
+}
+
+void g(int b) {
+        printf("g function, b = %d\n", b);
+}
+
+
+int main (int argc, char *argv[]) {
+    void f(int a);
+    void g(int b);
+
+    void (*pf)(int) = &f;
+    pf(10); // Вызывается f(10)
+
+    pf = &g;
+    pf(20); // Вызывается g(20)
+
+    return 0;
+}
